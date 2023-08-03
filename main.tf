541aad8ae2c201493df237bd6687436494e6dcd6
Maciej Kowalski <maciej.ko1444@gmail.com>
2023-08-03 11:00:51 +0200

    Bugfix & now it creates new EC2 instance when applied

--- main.tf
+++ main.tf
@@ -5,6 +5,7 @@ provider "aws" {
 resource "aws_instance" "example" {
     ami = "ami-0989fb15ce71ba39e"
     instance_type = "t3.micro"
+    vpc_security_group_ids = [aws_security_group.instance.id]
 
     user_data = <<-EOF
                 #!/bin/bash
@@ -21,7 +22,7 @@ resource "aws_instance" "example" {
 
 resource "aws_security_group" "instance" {
     name = "terraform-example-instance"
-    
+
     ingress {
         from_port = 8080
         to_port = 8080

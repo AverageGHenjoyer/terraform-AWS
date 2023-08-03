9705de8b34b1f8112a73ffbc2b7e2cfde47c5313
Maciej Kowalski <maciej.ko1444@gmail.com>
2023-08-03 10:49:27 +0200

    Added an ingress traffic

--- main.tf
+++ main.tf
@@ -6,8 +6,27 @@ resource "aws_instance" "example" {
     ami = "ami-0989fb15ce71ba39e"
     instance_type = "t3.micro"
 
+    user_data = <<-EOF
+                #!/bin/bash
+                echo "Hello, World" > index.html
+                nohup busybox httpd -f -p 8080 &
+                EOF
+
+    user_data_replace_on_change = true
+
     tags = {
         Name = "terraform-example"
+    }
+}
+
+resource "aws_security_group" "instance" {
+    name = "terraform-example-instance"
+    
+    ingress {
+        from_port = 8080
+        to_port = 8080
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
     }
 }
 
